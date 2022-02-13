2ebbed29e2485cc45a242e941c1d70ef351c190a
zetatez <zetatez@icloud.com>
2022-02-13 18:44:06 +0800

    cmt

--- cron_msg.sh
+++ cron_msg.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env sh
 
 # crontab -e
-# */3 * * * * sh /home/lorenzo/suckless/arch-dwmstatus/cron_msg.sh >> /dev/null
+# * * * * * sh /home/lorenzo/.suckless/arch-dwmstatus/cron_msg.sh >> /dev/null
 
 FILEPATH=/home/lorenzo/.notification.msg
 :> $FILEPATH
 
-for var in `seq 1 30`; do
-    # taskwarrior
-    task list|grep -v "ID\|---\|task\|^$"|sed "s/[ ][ ]*/ /g"|head -n 1 > $FILEPATH
+# any msgs can be appended on
+task|grep -ve "^$\| task\|--" > /tmp/notification.msgs
 
-    # email
-    :
 
-    sleep 2
+ct=0
+while [ $ct -lt 10 ];
+do
+    while read line;
+    do
+        echo "$line" > $FILEPATH
+        let ct=ct+1
+        sleep 6
+    done < /tmp/notification.msgs
 done
-
-
 
